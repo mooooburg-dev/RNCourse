ac2d9e8bbce1129068edb89b466162f90f646b72
mooooburg-dev <jeongmupark@gmail.com>
2022-12-12 19:23:34 +0900

    레이아웃 생성에 플렉스 박스 사용하기

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
-import { Fragment } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <Fragment>
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-        <Text>Hello World</Text>
-        <Button title="Submit" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
       </View>
-    </Fragment>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
   },
 });
