f54c8f2472e532f8e91a1c35321a60b62fed0e18
mooooburg-dev <jeongmupark@gmail.com>
2023-01-26 18:17:30 +0900

    ios 테두리 두께 변경

--- components/ui/Title.ios.js
+++ components/ui/Title.ios.js
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     // borderWidth: Platform.OS === 'android' ? 2 : 0,
     // borderWidth: Platform.select({ ios: 0, android: 2 }),
-    borderWidth: 10,
+    borderWidth: 2,
     borderColor: 'white',
     padding: 12,
     maxWidth: '80%',
